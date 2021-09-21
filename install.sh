b3452592f32c3934b63e073a91856105b8115225
Rokibul Hasan <rokibul-hasan@users.noreply.github.com>
2021-09-21 12:15:34 +0600

    update shell

--- install.sh
+++ install.sh
@@ -9,3 +9,11 @@ apt-get -y install php mysql-server apache2;
 
 #installl php myadmin
 apt-get -y install phpmyadmin;
+
+apt install php-mbstring php-zip php-gd php-json php-curl -y;
+
+phpenmod mbstring;
+
+systemctl restart apache2;
+
+apt-get install freeradius freeradius-mysql freeradius-utils -y;
